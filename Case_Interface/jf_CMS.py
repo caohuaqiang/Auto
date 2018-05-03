3cb63f00e06966cbe0f9e52e7dc3a04178d3603e
caohuaqiang <342473195@qq.com>
2018-05-03 17:14:27 +0800

    CMS接口测试用例3.0超级强化版

--- Case_Interface/jf_CMS.py
+++ Case_Interface/jf_CMS.py
@@ -15,6 +15,7 @@ class CMS(unittest.TestCase):
         self.session = requests.session()
         self.ym = 'http://139.196.107.14:9000'
 
+    # @unittest.skip('跳过sms图片查询列表')
     def test_picture_query(self):
         """图片查询"""
         url = self.ym + '/cms/images/queryEnableImages'
@@ -75,10 +76,49 @@ class CMS(unittest.TestCase):
                     raise Exception(err)
             print('===============================================以上是【' + v + '】的内容=========================================================')
 
-    # def test_article_query(self):
-    #     """文章查询"""
-    #     url = self.ym + '/cms/article/query/enableArticles'
-    #     data
+    def test_article_query(self):
+        """文章查询"""
+        url = self.ym + '/cms/article/query/enableArticles'
+        menuCode = {'site_notice': '网站公告',                  # 图片类型
+                    'media_report': '媒体报道',
+                    }
+        data_search_article = {'menuCode': '',
+                               'pageIndex': '3',
+                               'pageSize': '5',
+                               }
+
+        for k, v in menuCode.items():
+            print(v + ': ')
+            data_search_article['menuCode'] = k
+            res = self.session.request(method='get', url=url, params=data_search_article)
+            self.assertEqual(200, res.status_code)
+            articles = res.json()['data'][k]['articleDtoList']
+            num = 0
+            for article in articles:
+                # pprint(article)
+                # print('-----------------------------------------------------------------------------------------------------------------------------')
+                num += 1                                                                # 统计出 接口返回的列表内有几条数据
+            sql = "SELECT COUNT(*) AS NUM from tn_cms_article where menu_code = %s;"
+            with UseDataBase() as cursor:
+                cursor.execute(sql, args=(k, ))
+                contents = cursor.fetchall()
+            sql_num = contents[-1]['NUM']                                   # sql_num:数据库中的条数；num:接口返回的条数
+            print('sql_num数据库中的总条数：', sql_num)
+
+            pagesize = int(data_search_article['pageSize'])                 # 每页想要的条数
+            pageindex = int(data_search_article['pageIndex'])               # 接口传参-【想要】显示的是第几页
+            sql_index = int(sql_num/pagesize)                               # 算出能全显示条数的页数
+            print('sql_index能全显示条数的页数:', sql_index)
+            print('pagesize想要显示几条数据：', pagesize)
+            yushu = int(sql_num % pagesize)
+            print('余数:', yushu)
+
+            if yushu != 0 and (pageindex - sql_index) == 1:      # 逻辑：以数据库查出14条数据为例，请求参数为每页5条数据，那么第一第二页都能按照要求显示5条，但最后一页就只能显示最后剩下的4条
+                self.assertEqual(yushu, num)
+            elif (pageindex - sql_index) > 1:                   # 超过最后一页的页数时，空列表
+                self.assertEqual(0, num)
+            else:
+                self.assertEqual(pagesize, num)
 
 
 if __name__ == '__main__':
--- tiaoshi/hahahahahaha.py
+++ tiaoshi/hahahahahaha.py
@@ -11,10 +11,11 @@ def chq(data):
     # pprint(res.json())
 
 
-def article(data):
+def search_article(data):
     session = requests.session()
     url = 'http://139.196.107.14:9000/cms/article/query/enableArticles'
-
+    res = session.request(method='get', url=url, params=data)
+    return res
 
 
 
@@ -22,18 +23,60 @@ def article(data):
 
 
 if __name__ == '__main__':
-    data = {'imageTypes': 'find_icon_mb',
-            'source': ''}
-    print('传参： ',data)
+    # data = {'imageTypes': 'find_icon_mb',
+    #         'source': ''}
+    # print('传参： ',data)
+    #
+    # print(chq(data))
+    # print('=================================上面是完整json=================================================')
+    # dt = chq(data)['data']
+    # if dt:
+    #     try:
+    #         contents = dt[data['imageTypes']]
+    #         for content in contents:
+    #             pprint(content)
+    #             print('------------------------------------------------------------------------------------------------')
+    #     except Exception:
+    #         raise Exception('数组越界')
+    tp = 'site_notice'
+    data = {'menuCode': tp,
+            'pageIndex': '4',
+            'pageSize': '5',
+            }
+    res = search_article(data)
 
-    print(chq(data))
-    print('=================================上面是完整json=================================================')
-    dt = chq(data)['data']
-    if dt:
-        try:
-            contents = dt[data['imageTypes']]
-            for content in contents:
-                pprint(content)
-                print('------------------------------------------------------------------------------------------------')
-        except Exception:
-            raise Exception('数组越界')
+    pprint(res.json())
+
+    # articles = res.json()['data'][tp]['articleDtoList']
+    # n = 0
+    # for article in articles:
+    #     pprint(article)
+    #     print('-----------------------------------------------------------------------------------------------------------------------------')
+    #     n += 1
+    # print(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
